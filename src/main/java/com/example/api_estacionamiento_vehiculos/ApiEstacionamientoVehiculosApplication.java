3d456944e1e845eac5baa61f6ae44392b4f28b60
aldoG <agallardoa2@upao.edu.pe>
2023-04-17 02:00:28 -0500

    commit 1

---
+++ src/main/java/com/example/api_estacionamiento_vehiculos/ApiEstacionamientoVehiculosApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_estacionamiento_vehiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEstacionamientoVehiculosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiEstacionamientoVehiculosApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/api_estacionamiento_vehiculos/models/Car.java
@@ -0,0 +1,2 @@
+package com.example.api_estacionamiento_vehiculos.models;public class Car {
+}
